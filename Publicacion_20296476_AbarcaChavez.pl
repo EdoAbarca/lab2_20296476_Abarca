68867e0da9a17235e89fc46833bbbac902f406b0
EdoAbarca <eduardo.abarca.c@usach.cl>
2021-06-02 21:50:21 -0400

    3er commit

--- Publicacion_20296476_AbarcaChavez.pl
+++ Publicacion_20296476_AbarcaChavez.pl
@@ -1,10 +1,23 @@
 % TDA Publicacion
 % Composicion: [IdP, FechaP, CuentaCreadoraP, ContenidoP,
-% CuentaQueComparteP, CuentaDestinoP] -> [integer, TDA Fecha, string,
-% string, string, string] *SUJETO A CAMBIOS*
+% ListaCompartidos] -> [integer, TDA Fecha, string,
+% string, list] *SUJETO A CAMBIOS*
 
 % Constructores
+crearPublicacion(IdP, FechaP, CuentaCreadoraP, ContenidoP, ListaCompartidos, P) :- P = [IdP, FechaP, CuentaCreadoraP, ContenidoP, ListaCompartidos].
+
 % Selectores
+getIdP(P, Id) :- [Id|_] = P.
+getFechaP(P, F) :- [_|[F|_]] = P.
+getCuentaP(P, C) :- [_|[_|[C|_]]] = P.
+getContenidoP(P, Cont) :- [_|[_|[_|[Cont|_]]]] = P.
+getCompartidosP(P, Comp) :- [_|[_|[_|[_|[Comp|_]]]]] = P.
+
 % Pertenencia
+esPublicacion(P) :- not(esLista(P)), !, fail.
+esPublicacion(P) :- largo(P, L), L \== 5, !, fail.
+esPublicacion(P) :- getIdP(P, Id), getFechaP(P, F), getCuentaP(P, C), getContenidoP(P, Cont), getCompartidosP(P, Comp),
+    integer(Id), esFecha(F), string(C), string(Cont), esLista(Comp).
+
 % Modificadores
 % Otros
--- Reaccion_20296476_AbarcaChavez.pl
+++ Reaccion_20296476_AbarcaChavez.pl
@@ -4,7 +4,21 @@
 % string] *SUJETO A CAMBIOS*
 
 % Constructores
+crearReaccion(IdPR, IdR, FechaR, CuentaCreadoraR, TipoR, ContenidoR, R) :- R = [IdPR, IdR, FechaR, CuentaCreadoraR, TipoR, ContenidoR].
+
 % Selectores
+getIdPR(R, IdPR) :-             [IdPR|_] = R.
+getIdP(R, IdR) :-               [_|[IdR|_]] = R.
+getFechaR(R, Fecha) :-          [_|[_|[Fecha|_]]] = R.
+getCuentaR(R, Cuenta) :-        [_|[_|[_|[Cuenta]]]] = R.
+getTipoR(R, TipoR) :-           [_|[_|[_|[_|[TipoR|_]]]]] = R.
+getContenidoR(R, ContenidoR) :- [_|[_|[_|[_|[_|[ContenidoR|_]]]]]] = R.
+
 % Pertenencia
+esReaccion(R) :- not(esLista(R)), !, fail.
+esReaccion(R) :- largo(R, L), L \== 6, !, fail.
+esReaccion(R) :- getIdPR(R, IdPR), getIdP(R, IdR), getFechaR(R, Fecha), getCuentaR(R, Cuenta), getTipoR(R, TipoR), getContenidoR(R, ContenidoR),
+    integer(IdPR), integer(IdR), esFecha(Fecha), string(Cuenta), string(TipoR), string(ContenidoR).
+
 % Modificadores
 % Otros
